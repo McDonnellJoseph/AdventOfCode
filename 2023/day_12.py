ac81b8b20a8d830adb6a0ff4f81c8aea84cf1f7c
McDonnellJoseph <joseph.mcd@outlook.fr>
2023-12-13 08:39:24 +0100

    [WIP] Day 12

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -14,23 +14,57 @@ def parse_group(record, cond):
     return True
 
 
-def backtrack(obj, cond):
-    if reject(obj, cond):
-        return
-    elif accept(obj, cond):
-        return obj, cond
-
-
 PATTERN = re.compile(r"#+")
 
 
-def enumerate_condition(string, conditions):
-    to_replace = string.index("?")
+def enumerate_condition(string, conditions, count):
+    print("we are here")
+    print(string)
     groups = list(PATTERN.finditer(string))
+    # breaking condition no more ? in string and the count matches the conditions we increase the count by one
+    if "?" not in string:
+        print("not here")
+        if len(groups) == len(conditions):
+            truths = [
+                len(groups[i].span()) == conditions[i] for i in range(len(conditions))
+            ]
+            if sum(truths) == len(conditions):
+                print("true")
+                return 1
+        return 0
+    to_replace = string.index("?")
+    left = string[:to_replace] + "." + string[to_replace + 1 :]
+    right = string[:to_replace] + "#" + string[to_replace + 1 :]
+    # If there are groups
+    preceed = [group for group in groups if group.span()[1] < to_replace]
+    for i in range(len(preceed)):
+        if i < len(preceed) and len(preceed[i].span()) != conditions[i]:
+            return 0
+        else:
+            print("toto")
+            if len(preceed[i]) == conditions[i]:
+                count += enumerate_condition(
+                    left[to_replace + 1 :], conditions[i + 1 :], count
+                )
+            elif len(preceed[i]) < conditions[i] and preceed[i].span()[1] == (
+                to_replace - 1
+            ):
+                count += enumerate_condition(
+                    right[preceed[i].span()[0] :], conditions[i:], count
+                )
+            else:
+                return 0
+    if len(preceed) == 0:
+        count += enumerate_condition(left[to_replace:], conditions, count)
+        count += enumerate_condition(right[to_replace:], conditions, count)
 
-    if len(groups) > conditions:
-        return False
+    return count
 
+
+print(enumerate_condition("?#?#?#?#?#?#?#?", [1, 3, 1, 6], 0))
+
+
+"""
 
 def resolve_tree(record, cond):
     # Breaking condition: We have only # and ?
@@ -55,3 +89,4 @@ for line in input.splitlines():
         pass
     else:
         pass
+"""
