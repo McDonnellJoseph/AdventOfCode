c7859c75230378300403ddaf8f28f65ebf82fa79
Joseph McDonnell <joseph.mcd@outlook.fr>
2024-11-18 22:39:35 +0100

    temp: before delete

--- 2023/day_17.py
+++ 2023/day_17.py
@@ -4,6 +4,56 @@ with open("input.txt") as f:
 lines = input.splitlines()
 G = dict()
 
+"""
+unvisited_nodes = set()
+distance_from_start = dict()
+# Create graph
+for i in range(len(lines)):
+    for j in range(len(lines[0])):
+        weight = int(lines[i][j])
+        G[(i, j)]=weight
+        unvisited_nodes.add((i, j))
+        if (i, j) == (0, 0):
+            distance_from_start[(i, j)] = 0
+        else:
+            distance_from_start[(i, j)] = float("inf")
+
+assert len(lines) == len(lines[0])
+# Attempt with Floyd Warshall
+n_vertices = len(lines) * len(lines)
+# Set cost of going from one edge to another 
+dist = [[float("inf") for i in range(n_vertices)] for j in range(n_vertices)]
+
+for vertice in G:
+    weight = G[vertice]
+    print(vertice)
+    if vertice[0] > 0:
+        dist[vertice[0] * len(lines) + vertice[1]][(vertice[0] -1)*len(lines) + vertice[1]] = weight
+    if vertice[0] < len(lines) -1:
+        dist[vertice[0] * len(lines) + vertice[1]][(vertice[0] +1) * len(lines) + vertice[1]] = weight
+    if vertice[1] > 0:
+        dist[vertice[0] * len(lines) + vertice[1]][vertice[0] * len(lines) + vertice[1] -1]= weight
+    if vertice[1] < len(lines) -1:
+        dist[vertice[0] * len(lines) + vertice[1]][vertice[0]  * len(lines)+ vertice[1] + 1] = weight
+
+    dist[vertice[0] * len(lines) + vertice[1]][vertice[0]*len(lines)+vertice[1]] = 0
+
+for k in range(n_vertices):
+    for i in range(n_vertices):
+        for j in range(n_vertices):
+            print("toto")
+print("Size of dist ", len(dist) * len(dist[0]))
+#dist = [[G[(i, j)] for j in range(len(lines[0]))] for i in range(len(lines))]
+
+print(dist[0])
+for i in range(len(lines[0])):
+    dist[i][i] = 0
+for edge in G:
+    pass
+
+
+
+"""
 # 1. Create Graph
 for i in range(len(lines)):
     for j in range(len(lines[0])):
@@ -24,64 +74,148 @@ for i in range(len(lines)):
 
 import sys
 
-sys.setrecursionlimit(int(1e6))
+sys.setrecursionlimit(int(1e9))
+from functools import lru_cache
 
+def get_nodes(G, node, predecessors):
+    path = [node]
 
-def get_nodes(G, path):
-    last = path[-1]
-    candidates = list(G[last])
-    print("These", candidates)
+    if predecessors[node]:
+        last = predecessors[node]
+        path.append(last)    
+        
+        if predecessors[last]:
+            second_last = predecessors[last]
+            path.append(second_last)
+
+    candidates = list(G[node])
+
+    # remove candidate that would involve u-turn 
+    candidates.remove(path[-1])
+
     # if we have at least 2 pre-decessors
-    if len(path) > 2:
+    if len(path) == 3:
         # compute difference
-        diff = (last[0] - path[-3][0], last[1] - path[-3][1])
+        diff = (last[0] - predecessors[-3][0], last[1] - predecessors[-3][1])
 
-        if abs(diff[0]) > 2 or abs(diff[1]) > 2:
+        # Moving horizontally
+        if abs(diff[0]) == 2:
             # Moving right
-            if last[0] > path[-3][0]:
-                assert last[0] - 3 == path[-3][0]
-                caca = (last[0] + 1, last[1])
-                candidates.remove(caca)
+            if diff[0] > 0 and node[0] < len(lines)-1:
+                candidates.remove((node[0]+1, node[1]))
             # Moving left
-            elif last[0] < path[-3][0]:
-                assert last[0] + 3 == path[-3][0]
-                caca = (last[0] - 1, last[1])
-                candidates.remove(caca)
-            # Moving up
-            elif last[1] < path[-3][1]:
-                assert last[1] + 3 == path[-3][1]
-                caca = (last[0], last[1] - 1)
-                candidates.remove(caca)
+            elif diff[0] < 0:
+                if node[0] > 0:
+                    candidates.remove((node[0]-1, node[1]))
+
+        # Moving Vertically
+        elif abs(diff[1])==2:
             # Moving down
-            elif last[1] > path[-3][1]:
-                assert last[1] - 3 == path[-3][1]
-                caca = (last[0], last[1] + 1)
-                candidates.remove(caca)
-    if path[-1] in candidates:
-        candidates.remove(path[-1])
+            if diff[1] > 0 and node[1] < len(lines)-1:
+                candidates.remove((node[0], node[1]+1))
+            # Moving up
+            elif diff[1] < 0 and node[1] > 0:
+                candidates.remove((node[0], node[1]-1))           
+
     return candidates
 
 
-def brute_force(G, path, total, start, dest):
-    # We are visiting a node let's add it to the path
-    path.append(start)
+def min_dict(d):
+    return min(d, key=d.get)
+def djikstra(G):
+    Q  = [g for g in G]
+    dist = {g: float("inf") for g in G}
+    prev = {g: None for g in G}
+
+    dist[(0,0)] = 0
+
+    while Q:
+        u = min_dict(dist)
+        Q.remove(u)
+
+        for neighbor in get_nodes(G, node, prev):
+            alt = dist[u] + G[neighbor]
+
+
+def iterative(G, start, dest):
+    all_predss = [([start], 0)]
+    total_reached = []
+    best_total = 140
+    
+    while all_predss:
+        # Explore
+        candidate_min_total = {}
+        new_predss = []
+        for predecessors, total in all_predss:
+            candidates = get_nodes(G, predecessors)
+            print("candidates", predecessors, candidates)
+            for candidate in candidates:
+                candidate_total = total + G[predecessors[-1]][candidate]
+
+                if candidate == dest:
+                    print("Reached Destination")
+                    total_reached.append(candidate_total)
+                    print(candidate_total)
+                    assert False
+                    if best_total > candidate_total:
+                        print(candidate_total)
+                        best_total = candidate_total
+
+                elif candidate not in candidate_min_total.keys():
+                    candidate_min_total[candidate] = (predecessors, candidate_total)
+                    new_predss.append((predecessors + [candidate], candidate_total))
+
+                elif candidate_min_total[candidate][1] > candidate_total:
+                    print(all_predss)
+                    print("This is the candidate", candidate)
+                    print(candidate_min_total[candidate])
+                    candidate_min_total[candidate] = (predecessors, candidate_total)
+                    new_predss.append((predecessors + [candidate], candidate_total))
+          
+        all_predss = new_predss
+        for predecessors in all_predss:
+            assert len(predecessors[0]) == len(all_predss[0][0])
+    return best_total
+
+
+print(iterative(G, (0, 0), (13, 13)))
+def brute_force(G, predecessors, total, start, dest, best):
+    # We are visiting a node let's add it to the predecessors
+    
+    predecessors.append(start)
     # Stopping condition
     if start == dest:
-        return path, total
-    elif start in path:
-        return None, None
-
+        print(predecessors)
+        print(len(predecessors), total)
+        if total < best:
+            best = total
+            BEST_TOTAL = total
+        print(total)
+        end_reached.append(total)
+        return predecessors, total, best
     else:
         # Retreive nodes we can travel to
-        allowed = get_nodes(G, path)
+        allowed = get_nodes(G, predecessors)
         alternatives = []
         totals = []
         for node in allowed:
-            total += G[start][node]
-            new_p, new_t = brute_force(G, path, total, node, dest)
+            assert max(abs(node[0] - start[0]), abs(node[1] -start[1])) == 1
+            new_total = total + G[start][node]
+            new_preds = predecessors.copy() 
+
+            if node in predecessors or new_total > best:  
+                continue
+            if new_total > best:
+                assert False
+            new_p, new_t, maybe_best = brute_force(G, new_preds, new_total, node, dest, best)
+            if maybe_best < best:
+                best = maybe_best
             alternatives.append(new_p)
-            totals.append(total)
-        return alternatives, totals
+            totals.append(new_t)
+        return alternatives, totals, best
 
+#print(brute_force(G, [], 0, (0, 0), (len(lines) - 1, len(lines[0]) - 1), 103) )
+#print(brute_force(G, [], 0, (0, 0), (6, 6), 60) )
 
-print(brute_force(G, [], 2, (0, 0), (len(lines) - 1, len(lines[0]) - 1)))
+#print(end_reached)
+#print("#################")
