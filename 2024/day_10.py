95d4b16ed6f0ba88856cc01b0eb5f2f06af071f8
Joseph McDonnell <joseph.mcd@outlook.fr>
2024-12-10 07:50:52 +0100

    both parts

--- 2024/day_10.py
+++ 2024/day_10.py
@@ -13,7 +13,19 @@ for i in range(len(input)):
 
 moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def step(pos, reached_set):
+def step_p2(pos, reached_count):
+    current_val = topo[pos]
+    for move in moves:
+        next_pos = (pos[0] + move[0], pos[1] + move[1])
+        if next_pos in topo:
+            if topo[next_pos] - current_val == 1:
+                if topo[next_pos] == 9:
+                    reached_count += 1
+                else:
+                    reached_count = step_p2(next_pos, reached_count)
+    return reached_count
+
+def step_p1(pos, reached_set):
     current_val = topo[pos]
     for move in moves:
         next_pos = (pos[0] + move[0], pos[1] + move[1])
@@ -22,12 +34,15 @@ def step(pos, reached_set):
                 if topo[next_pos] == 9:
                     reached_set.add(next_pos)
                 else:
-                    reached_count = step(next_pos, reached_set)
+                    reached_set = step_p1(next_pos, reached_set)
     return reached_set
 # for each trailhead
-total = 0 
+total_p1 = 0 
+total_p2 = 0
 for trailhead in trailheads:
-    total += len(step(trailhead, set()))
+    total_p1 += len(step_p1(trailhead, set()))
+    total_p2 += step_p2(trailhead, 0)
 
 
-print(total)
+print("part 1", total_p1)
+print("part 2", total_p2)
