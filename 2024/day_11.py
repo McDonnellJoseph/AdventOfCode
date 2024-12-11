54cb0cdbe10bfd6b981876ca0f84ee7c9e5044d0
Joseph McDonnell <joseph.mcd@outlook.fr>
2024-12-11 08:28:22 +0100

    p1 day 11

---
+++ 2024/day_11.py
@@ -0,0 +1,58 @@
+with open("input.txt") as f:
+    input = f.read()
+
+
+line = [int(number) for number in input.split()]
+
+
+
+def blink(line):
+    # For each 0 
+    index_offset = 0
+    for i in range(len(line)):
+        i += index_offset
+        elem = line[i]
+
+        # If stone is zero it turns to 1
+        if elem == 0:
+            line[i] = 1
+
+        elif len(str(elem)) % 2 == 0:
+            elem_str = str(elem)
+            left, right = int(elem_str[:len(elem_str)//2]), int(elem_str[len(elem_str)//2:])
+
+            line.insert(i, left)
+            line.insert(i+1, right)
+            del line[i+2]
+            index_offset += 1
+
+        else:
+            line[i] = elem * 2024
+
+    return line 
+
+from functools import lru_cache
+
+# For each 0 
+@lru_cache(max_size=None)
+def digit_transform(digit, n_transformations)
+    while len(str(digit)) % 2 != 0:
+        if digit == 0:
+            digit = 1
+        else:
+            digit = digit * 2024
+        n_transformations += 1
+    elem_str = str(digit)
+    left, right = int(elem_str[:len(elem_str)//2]), int(elem_str[len(elem_str)//2:])
+    return left, right, n_transformations + 1
+
+
+for l in line:
+    n_transformations = 0
+    while n_transformations < 75:
+        left, right, n_transformations = digit_transform(digit, n_transformations) 
+
+
+for i in range(25):
+    line = blink(line)
+print(len(line))
