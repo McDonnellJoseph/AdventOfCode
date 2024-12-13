35e311646e8314ca5f9efb9eb7e3a190265b863e
Joseph McDonnell <joseph.mcd@outlook.fr>
2024-12-13 08:33:59 +0100

    z3 solver :(

--- 2024/day_13.py
+++ 2024/day_13.py
@@ -1,22 +1,24 @@
 import numpy as np 
 import re 
+from z3 import Ints, Solver, sat, ToInt
 with open("input.txt") as f:
     input = f.read()
 
 machines = input.split("\n\n")
 total_p1 = 0
+x, y = Ints('x y')
 for machine in machines:
     lines = machine.splitlines()
     ax, ay = [int(_) for _ in re.findall(r'\d+',lines[0])]
     bx, by = [int(n) for n in re.findall(r'\d+',lines[1])]
-    x, y = [int(n) for n in re.findall(r'\d+',lines[2])]
-
-    a_arr = np.array([[ax, bx], [ay, by]])
-    b_arr = np.array([x,y])
-    sol = np.linalg.solve(a_arr, b_arr)
+    ans_a, ans_b = [int(n) for n in re.findall(r'\d+',lines[2])]
+    solver = Solver()
+    solver.add(*[ax * x + bx * y == ans_a+10000000000000, ay * x + by * y== ans_b+10000000000000, x > 0, y > 0])
     # Test if it works
-    answ_a, answ_b = int(sol[0]), int(sol[1])
+    if solver.check() == sat:
+        print("here")
+        model = solver.model()
+        print("model x ", model[x])
+        total_p1 += 3 * model[x].as_long() + model[y].as_long()
 
-    if ax *answ_a + bx * answ_b == x and ay * answ_a + by *answ_b == y:
-        total_p1 += 3 * int(answ_a) + int(answ_b)
 print(total_p1)
