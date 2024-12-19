2b3538f197406fa579c055ddccda249d2102fc8c
Joseph McDonnell <joseph.mcd@outlook.fr>
2024-12-19 08:32:07 +0100

    part 2 ongoing

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -25,9 +25,40 @@ def is_possible(goal, patterns):
             if left and right:
               return True
         return False
+
+@lru_cache(maxsize=None)
+def count_possible(goal, patterns):
+    counts = 0
+    if goal in patterns:
+        print("Reached goal")
+        return 1
+    elif len(goal) == 1:
+        return 0
+    else:
+        for i in range(1, len(goal)//2+1):
+            left_counts = 0 
+            rigth_counts = 0
+            left_counts = count_possible(goal[:i], patterns)
+            print("left", goal[:i], "patterns", patterns)
+            print("Left count", left_counts)
+            if left_counts > 0:
+                rigth_counts = count_possible(goal[i:], patterns)
+                print("right", goal[i:], "patterns", patterns)
+                print("right count", rigth_counts)
+            if left_counts > 0 and rigth_counts > 0:
+                print("Left + Right")
+                counts += left_counts * rigth_counts
+                print(counts)
+                
+        return counts
+
 count_p1 = 0
+count_p2 = 0
 for i, goal in enumerate(targets):
     print(i)
     if is_possible(goal, patterns):
         count_p1 += 1
+    count_p2 += count_possible(goal, patterns)
+
 print("Part1", count_p1)
+print("Part2", count_p2)
