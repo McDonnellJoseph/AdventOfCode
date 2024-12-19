66a9de856d16da09a17145634314d00e5738f2e4
jmcdonnell <jmcdonnell@fieldbox.ai>
2024-12-19 16:05:15 +0100

    part 2

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+
 with open("input.txt") as f:
     input = f.read()
 
@@ -8,6 +9,7 @@ targets = targets.splitlines()
 
 patterns = tuple([p.strip() for p in patterns.split(",")])
 
+
 @lru_cache(maxsize=None)
 def is_possible(goal, patterns):
     if goal in patterns:
@@ -16,41 +18,33 @@ def is_possible(goal, patterns):
         return False
     else:
         for i in range(1, len(goal)):
-            left= False
+            left = False
             rigth = False
 
             left = is_possible(goal[:i], patterns)
             if left:
                 right = is_possible(goal[i:], patterns)
             if left and right:
-              return True
+                return True
         return False
+
 
 @lru_cache(maxsize=None)
 def count_possible(goal, patterns):
     counts = 0
+    # We have one match
     if goal in patterns:
-        print("Reached goal")
-        return 1
-    elif len(goal) == 1:
-        return 0
-    else:
-        for i in range(1, len(goal)//2+1):
-            left_counts = 0 
-            rigth_counts = 0
-            left_counts = count_possible(goal[:i], patterns)
-            print("left", goal[:i], "patterns", patterns)
-            print("Left count", left_counts)
-            if left_counts > 0:
-                rigth_counts = count_possible(goal[i:], patterns)
-                print("right", goal[i:], "patterns", patterns)
-                print("right count", rigth_counts)
-            if left_counts > 0 and rigth_counts > 0:
-                print("Left + Right")
-                counts += left_counts * rigth_counts
-                print(counts)
-                
+        counts += 1
+    # Breaking condition
+    if len(goal) == 1:
         return counts
+
+    for i in range(1, len(goal)):
+        if goal[:i] in patterns:
+            rigth_counts = count_possible(goal[i:], patterns)
+            counts += 1 * rigth_counts
+    return counts
+
 
 count_p1 = 0
 count_p2 = 0
